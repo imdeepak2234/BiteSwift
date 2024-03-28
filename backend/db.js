5e0246336341fd41db0d337c6a7eb5373d605fc5
imdeepak2234 <imdeepak2234@gmail.com>
2024-03-28 15:30:18 +0530

    Initial commit

---
+++ backend/db.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+// const mongoDbClient = require("mongodb").MongoClient
+const mongoURI = "mongodb://127.0.0.1:27017/FoodDelivery" 
+mongoose.set('strictQuery', true)
+
+module.exports = function (callback) {
+    mongoose.connect(mongoURI, async (err, result) => {
+        // mongoDbClient.connect(mongoURI, { useNewUrlParser: true }, async(err, result) => {
+        if (err) console.log("---" + err)
+        else {
+            // var database =
+            console.log("connected to mongo")
+            const foodCollection = await mongoose.connection.db.collection("food_items");
+            foodCollection.find({}).toArray(async function (err, data) {
+                const categoryCollection = await mongoose.connection.db.collection("Categories");
+                categoryCollection.find({}).toArray(async function (err, Catdata) {
+                    callback(err, data, Catdata);
+
+                })
+            });
+            // listCollections({name: 'food_items'}).toArray(function (err, database) {
+            // });
+            //     module.exports.Collection = database;
+            // });
+        }
+    })
+};
